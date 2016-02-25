e8d0b76e3bf740e5fb7dc5dd42de9e48f2af7dec
Choi Chi Po <po@Chois-MacBook-Pro.local>
2016-02-25 04:00:54 -0800

    use autoreconf

--- deps/build_healpix.sh
+++ deps/build_healpix.sh
@@ -10,8 +10,18 @@ mkdir -p lib
 # patch the configure script to search the correct directory on Travis
 patch hpxconfig_functions.sh ../../hpxconfig_functions.patch
 
-bash ./configure --disable-dependency-tracking --disable-silent-rules << EOF
-EOF
+cd src/C/autotools
+autoreconf --install
+bash ./configure --disable-dependency-tracking --disable-silent-rules
+make install
+
+cd ../../cxx/autotools
+echo"AUTOMAKE_OPTIONS = subdir-objects" >> Makefile.am
+autoreconf --install
+bash ./configure --disable-dependency-tracking --disable-silent-rules
+make install
+
+cd ../../../
 
 # patch the C Makefile to link libcfitsio into libchealpix
 patch src/C/subs/Makefile ../../src_C_subs_Makefile.patch
