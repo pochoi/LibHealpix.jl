cc7747790db95ff60018dfcc064cdb4fb0839dbf
Choi Chi Po <po@campus-006-142.ucdavis.edu>
2016-02-26 21:46:20 -0800

    osx homebrew gcc

--- deps/build_healpix.sh
+++ deps/build_healpix.sh
@@ -10,21 +10,27 @@ mkdir -p lib
 # patch the configure script to search the correct directory on Travis
 patch hpxconfig_functions.sh ../../hpxconfig_functions.patch
 
-bash ./configure -L
+bash ./configure -L << EOF
 2
 gcc-5
 -O2 -Wall
 ar -rsv
-Y
-libcfitsio.a
-/usr/local/lib
-/usr/local/include
 y
+
+
+
 y
-5
-/usr/local/lib
-/usr/local/include
+n
+0
+EOF
+
+bash ./configure -L << EOF
 4
+
+
+5
+n
+0
 EOF
 
 # patch the C Makefile to link libcfitsio into libchealpix
